a79d2f1ab1183995a8eb4676b60084e38cb0529e
SHRemover <yiwang0527@icloud.com>
2017-12-18 15:06:37 +0800

    Initial Commit

---
+++ RulerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RulerDemo
+//
+//  Created by gs_sh on 2017/12/18.
+//  Copyright © 2017年 Remover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
