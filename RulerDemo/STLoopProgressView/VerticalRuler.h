9c560d43f607406f5a0c779f5c5041ab2fa1a702
SHRemover <yiwang0527@icloud.com>
2017-12-19 10:38:04 +0800

    VerticalRuler

---
+++ RulerDemo/STLoopProgressView/VerticalRuler.h
@@ -0,0 +1,13 @@
+//
+//  VerticalRuler.h
+//  RulerDemo
+//
+//  Created by gs_sh on 2017/12/18.
+//  Copyright © 2017年 Remover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VerticalRuler : UIView
+
+@end
---
+++ RulerDemo/STLoopProgressView/VerticalRuler.m
@@ -0,0 +1,21 @@
+//
+//  VerticalRuler.m
+//  RulerDemo
+//
+//  Created by gs_sh on 2017/12/18.
+//  Copyright © 2017年 Remover. All rights reserved.
+//
+
+#import "VerticalRuler.h"
+
+@implementation VerticalRuler
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
